b46c9be9f2f87bbefe7efeb767eac4ceafa77bdb
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-18 20:11:14 +0200

    Initial Commit

---
+++ ol-007-10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ol-007-10
+//
+//  Created by Vahtee Boo on 18.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ol-007-10/ol_007_10App.swift
@@ -0,0 +1,17 @@
+//
+//  ol_007_10App.swift
+//  ol-007-10
+//
+//  Created by Vahtee Boo on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ol_007_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
